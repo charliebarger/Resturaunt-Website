7befc2d65d050c2f70e0049acbf7ff9661e0fb71
charliebarger <charliebarger04@gmail.com>
2021-01-18 08:42:18 -0700

    initialized setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log(wizard)
+const grabDiv = document.getElementById("content");
+
+let header = document.createElement('header')
+
+grabDiv.appendChild(header)
+
+let resturauntName = document.createElement('h1')
+
+resturauntName.textContent = "C & C's Oyster Bar"
+header.appendChild(resturauntName)
+
+let main = document.createElement('main')
+
+grabDiv.appendChild(main)
+
+let imageWrapper = document.createElement('div')
+
+imageWrapper.classList = 'imageWrapper'
+
+main.appendChild(imageWrapper)
+
+
+
+let frontImage = document.createElement("img")
+
+frontImage.src = "./img/ben-stern-4n96lyJd2Xs-unsplash.jpg"
+
+imageWrapper.appendChild(frontImage)
